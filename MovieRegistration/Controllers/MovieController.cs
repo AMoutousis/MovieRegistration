92657f72f8a376d41df526452b58b2d5d810d8a9
Swoopsy <38360122+Swoopsy@users.noreply.github.com>
2020-08-22 17:27:26 -0400

    first commit

---
+++ MovieRegistration/Controllers/MovieController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MovieRegistration.Models;
+
+namespace MovieRegistration.Controllers
+{
+    public class MovieController : Controller
+    {
+        public IActionResult Index(Movie movie)
+        {
+            return View(movie);
+        }
+
+        public IActionResult Movie(Movie movie)
+        {
+            return View("../Movie/AddMovie", movie);
+        }
+    }
+}
---
+++ MovieRegistration/Models/Movie.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+namespace MovieRegistration.Models
+{
+    public enum Genre
+    { 
+        Scifi,
+        Horror,
+        Action,
+        Drama,
+        Romance
+    }
+    public class Movie
+    {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 2, ErrorMessage = "Must be between 2 and 50 characters!")]
+        public string Title { get; set; }
+        [Required]
+        public Genre Genre { get; set; }
+        [Required]
+        [ValidDate]
+        public DateTime Year { get; set; }
+        [Required]
+        public string Actors { get; set; }
+        [Required]
+        public string Directors { get; set; }
+    }
+}
